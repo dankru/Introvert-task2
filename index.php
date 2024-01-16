0878025e9182e4e6e809c553de0397c32259854f
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-16 15:32:12 +0700

    replaced comments with phpdoc, removed warnings

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 // This scripts gets all leads with transfered statuses from today to 30 days in the future and returns json object,
 // containing the amount of leads for each timestamp date, date as key, leads count as value
 
+use Introvert\ApiClient;
+
 require_once(__DIR__ . '/vendor/autoload.php');
 
 $apiKey = "23bc075b710da43f0ffb50ff9e889aed";
@@ -11,8 +13,14 @@ $api = new Introvert\ApiClient();
 $api->getConfig()->setHost('https://api.s1.yadrocrm.ru/tmp');
 $api->getConfig()->setApiKey('key', '23bc075b710da43f0ffb50ff9e889aed ');
 
-// Main function, returns map of leads by date: value
-function getLeadsByStatusAndDate($customFieldId, $statusArray, $api): array
+/**
+ * Gets leads and filters it by statuses, date and custom field id.
+ * @param int $customFieldId A custom field id.
+ * @param array<int> $statusArray array of needed statuses.
+ * @param Introvert\ApiClient $api APIclient for getting leads
+ * @return array<object>
+ */
+function getLeadsByStatusAndDate(int $customFieldId, array $statusArray, ApiClient $api): array
 {
 
     $dateToday = new DateTime(); // Y-m-d
@@ -27,7 +35,7 @@ function getLeadsByStatusAndDate($customFieldId, $statusArray, $api): array
         $fields = $lead["custom_fields"];
         // filter leads by date from chosen field
         $fields = array_filter($fields, fn($field) => $field["id"] === $customFieldId && $field["values"][0]["value"] > $dateToday->format('Y-m-d') . " 00:00:00"
-        && $field["values"][0]["value"] < $dateOffset);
+            && $field["values"][0]["value"] < $dateOffset);
         // if no fields with this date, continue
         if (count($fields) === 0) {
             continue;
@@ -47,18 +55,29 @@ function getLeadsByStatusAndDate($customFieldId, $statusArray, $api): array
     return $leadsByDate;
 }
 
-// Get all leads in a loop
-function getAllLeads($statusArray, $api): array
+/**
+ * Gets all leads in a loop
+ * @param array<int> $statusArray array of needed statuses.
+ * @param Introvert\ApiClient $api APIclient for getting leads
+ * @return array<object>
+ */
+function getAllLeads(array $statusArray, ApiClient $api): array
 {
     $limit = 100;
     $offset = 0;
     $leads = [];
     do {
-        // user id hardcoded 8967010
-        $leadsByPage = $api->lead->getAll(8967010, $statusArray, null, null, $limit, $offset);
-        $leads = array_merge_recursive($leads, $leadsByPage);
-        $count = count($leadsByPage["result"]);
-        $offset += $limit;
+        try {
+            // user id hardcoded 8967010
+            $leadsByPage = $api->lead->getAll(8967010, $statusArray, null, null, $limit, $offset);
+            $leads = array_merge_recursive($leads, $leadsByPage);
+            $count = count($leadsByPage["result"]);
+            $offset += $limit;
+        } catch (Exception $e) {
+            echo "Exception when getting leads: ", $e->getMessage(), PHP_EOL;
+            break;
+        }
+
     } while ($count >= $limit);
     return $leads;
 }
